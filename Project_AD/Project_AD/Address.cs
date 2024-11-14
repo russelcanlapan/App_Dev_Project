e06ecebe59e02d89c17f0de5db763a771ef3f7da
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-14 17:26:37 -0500

    Added fields and constructor for User,Member and Employee

--- Project_AD/Project_AD/Address.cs
+++ Project_AD/Project_AD/Address.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project_AD
 {
-    internal class Address
+    public class Address
     {
     }
 }
--- Project_AD/Project_AD/Member.cs
+++ Project_AD/Project_AD/Member.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Project_AD
 {
-    internal class Member
+    public class Member : User
     {
+        private static int memberIdCounter = 1;
+        public string MemberId { get; private set; }
+        public Membership MembershipType { get; set; }
+        public double Balance { get; set; }
+        public static List<Notification> Notifications { get; private set; } = new List<Notification>();
+
+        public Member(string fname, string lname, Address address, string phoneNumber, Membership membershipType, double balance)
+            : base(fname, lname, address, phoneNumber)
+        {
+            MemberId = memberIdCounter++.ToString();
+            MembershipType = membershipType;
+            Balance = balance;
+        }
+
+        public static void AddNotification(Notification notification)
+        {
+            Notifications.Add(notification);
+        }
     }
 }
