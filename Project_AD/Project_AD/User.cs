d20adf43966b3c95dce0453253f4349c5fb6b488
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-14 17:21:14 -0500

    Fixed gitignore

--- Project_AD/Project_AD/User.cs
+++ Project_AD/Project_AD/User.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Project_AD
 {
-    internal class User
+    public abstract class User
     {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public Address Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public User(string fname, string lname, Address address, string phoneNumber)
+        {
+            FName = fname;
+            LName = lname;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
     }
 }
+
+
