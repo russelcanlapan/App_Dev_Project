19f157043b3f00842956d2916fa269a66a4a9f3e
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-14 18:52:30 -0500

    Made GUI for form 1

--- Project_AD/Project_AD/Form1.Designer.cs
+++ Project_AD/Project_AD/Form1.Designer.cs
@@ -31,6 +31,11 @@ namespace Project_AD
         private void InitializeComponent()
         {
             languageButton = new Button();
+            titleLabel = new Label();
+            loginMemberButton = new Button();
+            loginEmployeeButton = new Button();
+            signupButton = new Button();
+            exitButton = new Button();
             SuspendLayout();
             // 
             // languageButton
@@ -43,15 +48,66 @@ namespace Project_AD
             languageButton.UseVisualStyleBackColor = true;
             languageButton.Click += languageButton_Click;
             // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Location = new Point(314, 83);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(67, 15);
+            titleLabel.TabIndex = 1;
+            titleLabel.Text = "Gym World";
+            // 
+            // loginMemberButton
+            // 
+            loginMemberButton.Location = new Point(314, 175);
+            loginMemberButton.Name = "loginMemberButton";
+            loginMemberButton.Size = new Size(75, 23);
+            loginMemberButton.TabIndex = 3;
+            loginMemberButton.Text = "Member Login";
+            loginMemberButton.UseVisualStyleBackColor = true;
+            // 
+            // loginEmployeeButton
+            // 
+            loginEmployeeButton.Location = new Point(314, 222);
+            loginEmployeeButton.Name = "loginEmployeeButton";
+            loginEmployeeButton.Size = new Size(75, 23);
+            loginEmployeeButton.TabIndex = 4;
+            loginEmployeeButton.Text = "Employee Login";
+            loginEmployeeButton.UseVisualStyleBackColor = true;
+            // 
+            // signupButton
+            // 
+            signupButton.Location = new Point(314, 273);
+            signupButton.Name = "signupButton";
+            signupButton.Size = new Size(75, 23);
+            signupButton.TabIndex = 5;
+            signupButton.Text = "Sign Up";
+            signupButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            exitButton.Location = new Point(314, 323);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(75, 23);
+            exitButton.TabIndex = 6;
+            exitButton.Text = "Exit";
+            exitButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(exitButton);
+            Controls.Add(signupButton);
+            Controls.Add(loginEmployeeButton);
+            Controls.Add(loginMemberButton);
+            Controls.Add(titleLabel);
             Controls.Add(languageButton);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -64,5 +120,10 @@ namespace Project_AD
         }
 
         private Button languageButton;
+        private Label titleLabel;
+        private Button loginMemberButton;
+        private Button loginEmployeeButton;
+        private Button signupButton;
+        private Button exitButton;
     }
 }
