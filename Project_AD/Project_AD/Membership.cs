5661b00a0104bc978b725ef37be4b3f75b14c240
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-14 17:38:12 -0500

    Implemented Membership class

--- Project_AD/Project_AD/Membership.cs
+++ Project_AD/Project_AD/Membership.cs
@@ -8,5 +8,29 @@ namespace Project_AD
 {
     public class Membership
     {
+        public MembershipType Type { get; set; }
+
+        public Membership(MembershipType type)
+        {
+            Type = type;
+        }
+
+        /// <summary>
+        /// Gets the price based on the payment frequency.
+        /// </summary>
+        /// <param name="frequencyType">Frequency of the payment, either "monthly" or "yearly".</param>
+        /// <returns>The price for the given frequency.</returns>
+        public double GetPrice(string frequencyType)
+        {
+            if (frequencyType.Equals("monthly", System.StringComparison.OrdinalIgnoreCase))
+            {
+                return Type.MonthlyPrice;
+            }
+            else if (frequencyType.Equals("yearly", System.StringComparison.OrdinalIgnoreCase))
+            {
+                return Type.YearlyPrice;
+            }
+            throw new System.ArgumentException("Invalid frequency: " + frequencyType);
+        }
     }
 }
