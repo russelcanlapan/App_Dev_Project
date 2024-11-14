49ccf1ae4ec789bce1b7795d0ecf94ee56344f3e
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-13 23:37:23 -0500

    Made classes public

--- Project_AD/Project_AD/Employee.cs
+++ Project_AD/Project_AD/Employee.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project_AD
 {
-    internal class Employee
+    public class Employee
     {
     }
 }
--- Project_AD/Project_AD/obj/Release/net8.0-windows/Project_AD.AssemblyInfo.cs
+++ Project_AD/Project_AD/obj/Release/net8.0-windows/Project_AD.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_AD")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94030b2ae1967e197ec2e6295e5c224ccd6a35e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_AD")]
