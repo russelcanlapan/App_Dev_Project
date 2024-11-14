f257c6bd717f0622b5ed2cc529af23f518bd6f1c
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-14 17:35:59 -0500

    Implemented Address class

--- Project_AD/Project_AD/Address.cs
+++ Project_AD/Project_AD/Address.cs
@@ -8,5 +8,19 @@ namespace Project_AD
 {
     public class Address
     {
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string ZipCode { get; set; }
+        public int StreetNumber { get; set; }
+
+        public Address(int streetNumber, string streetName, string city, string province, string zipCode)
+        {
+            StreetName = streetName;
+            City = city;
+            Province = province;
+            ZipCode = zipCode;
+            StreetNumber = streetNumber;
+        }
     }
 }
