ffa58ddcc856a2278a2c7b936c4e7e43c48e55a4
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-14 19:19:06 -0500

    Made GUI for MemberLogin form

---
+++ Project_AD/Project_AD/MemberLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_AD
+{
+    public partial class MemberLogin : Form
+    {
+        public MemberLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
