1343ed2c09acfde5e7cccca96cf16ede2868816b
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-14 19:46:46 -0500

    Implemented l10n for form 2(member login)

--- Project_AD/Project_AD/obj/Debug/net8.0-windows/Project_AD.AssemblyInfo.cs
+++ Project_AD/Project_AD/obj/Debug/net8.0-windows/Project_AD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_AD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3466ab8851195b21156b015296d637422ca0a631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36b0de76e9762a67ca12882751602d940a335d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_AD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_AD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
