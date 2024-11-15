9dcfc40a9bd501efce20c1520cdd9402e46dcc98
Russel Canlapan <russeljcanlapan@gmail.com>
2024-11-15 18:21:15 -0500

    Fix submit button in SignUp Form

--- Project_AD/Project_AD/SignUp.cs
+++ Project_AD/Project_AD/SignUp.cs
@@ -17,13 +17,44 @@ namespace Project_AD
             InitializeComponent();
             UpdateComponent();
 
-            regularButton.Checked = true;
-            monthlyButton.Checked = true;
+            regularRB.Checked = true;
+            monthlyRB.Checked = true;
         }
 
         private void submitButton_Click(object sender, EventArgs e)
         {
+            MembershipType membershipType = new MembershipType();
+            Membership membership = new Membership(membershipType);
+            string frequencyType = "";
 
+            if (regularRB.Checked) {
+                if (monthlyRB.Checked)
+                {
+                    frequencyType = "monthly";
+                }
+                else if (yearlyRB.Checked)
+                {
+                    frequencyType = "yearly";
+                }
+            } 
+            else if (premiumRB.Checked) {
+                membershipType = MembershipType.Premium;
+                if (monthlyRB.Checked)
+                {
+                    frequencyType = "monthly";
+                }
+                else if (yearlyRB.Checked)
+                {
+                    frequencyType = "yearly";
+                }
+            }
+            // Create a new User instance
+            Member newUser = new Member(firstNameTB.Text, lastNameTB.Text,
+                new Address(int.Parse(streetNumberTB.Text), streetNameTB.Text,
+                cityTB.Text, provinceTB.Text, zipcodeTB.Text), phoneNumberTB.Text, membership, membership.GetPrice(frequencyType));
+
+            // Display confirmation or handle the user object as needed
+            MessageBox.Show("User information saved successfully.\n Member ID: " + newUser.MemberId);
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
