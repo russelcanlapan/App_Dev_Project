5e72e24300d5a101d9ea689b7d9465d3451c9dc5
Rojahn Regorosa <rjonielm@gmail.com>
2024-11-15 23:12:47 -0500

    Fix small bug concerning balance in view profile while in french

--- Project_AD/Project_AD/MemberLogin.cs
+++ Project_AD/Project_AD/MemberLogin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,7 +56,7 @@ namespace Project_AD
                             memberData[7], // Province
                             memberData[8], // Zip Code
                             memberData[9], // Membership Type
-                            double.Parse(memberData[10]) // Balance
+                            double.Parse(memberData[10], CultureInfo.InvariantCulture) // Balance
                         );
 
                         // Show the ViewProfile form
